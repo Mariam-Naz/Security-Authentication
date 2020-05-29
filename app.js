8ee8b99ef35d579c0c82037caa57617a921e8e27
unknown <mariamnaz212@gmail.com>
2020-05-30 03:38:34 +0500

    level 4 bcrypt(hashing + salt)

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
 const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 app.use(express.static('public'));
@@ -17,10 +19,6 @@ const userSchema = new mongoose.Schema({
     email:String,
     password:String
 });
-
-//encryption-work
-
-userSchema.plugin(encrypt,{secret:process.env.TOP_SECRET, encryptedFields:['password']});
 
 const User = mongoose.model('User',userSchema);
 
@@ -36,29 +34,39 @@ app.get('/register',function(req,res){
 
 app.post('/register',function(req,res){
     const email = req.body.username;
-    const password = md5(req.body.password);
-    const newUser = new User({
-        email: email,
-        password: password
+    const password = req.body.password;
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: email,
+            password: hash
+        });
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.render('secrets');
+            }
+        });
     });
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            res.render('secrets');
-        }
     });
-});
+    
 app.post('/login',function(req,res){
     const email = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password
     User.findOne({email:email},function(err,found){
         if(err){
             console.log(err);
         }else{
-            if(found.password === password){
-                res.render('secrets');
-            }
+            bcrypt.compare(password, found.password, function(err, result) {
+                // result == true
+                if(result === true){
+                    res.render('secrets');
+                }else{
+                    res.send('incorrect password');
+                }
+            });
+               
         }
     })
 });
